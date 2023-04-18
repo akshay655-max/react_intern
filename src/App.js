6994470118980a1b461afe2d4ec3f5302525d611
akshay <akshaysuman129@gmail.com>
2023-04-18 14:32:53 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import styles from "./Styles/App.module.css";
 import ComponentA from "./Components/ComponentA";
-
+import { useState } from "react";
 function App() {
+  const [isTrue, setIsTrue] = useState(true);
   return (
-    <div>
-      <ComponentA />
+    <div className={styles.parent_container}>
+      <div className={styles.box_container}>
+        <div
+          id={styles.circle}
+          className={isTrue ? styles.yellow : styles.green}
+        >
+          <ComponentA />
+        </div>
+      </div>
+      <button
+        id={styles.color_change_btn}
+        className={isTrue ? styles.green : styles.yellow}
+        onClick={() => setIsTrue(!isTrue)}
+      >
+        Change Color
+      </button>
     </div>
   );
 }
--- src/Components/ComponentB.jsx
+++ src/Components/ComponentB.jsx
@@ -1,12 +1,14 @@
-
 import React from "react";
 import styles from "../Styles/ComponentB.module.css";
 
-function ComponentB({ cendol }) {
+function ComponentB(props) {
+  const { cendol, setCendol } = props;
   return (
     <div className={styles.parent_container}>
       <h1>{cendol}</h1>
-      <button>Click to increase counter</button>
+      <button onClick={() => setCendol(cendol + 10)}>
+        Click to increase counter
+      </button>
     </div>
   );
 }
